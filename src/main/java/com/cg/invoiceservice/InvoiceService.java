53e42832d0414f86c0d9bce695f3cd1788e5046d
AV-NASH <avinash21213434@gmail.com>
2020-10-26 22:58:17 +0530

    Through TDD calculate fare method has been created and tested

--- src/main/java/com/cg/invoiceservice/InvoiceService.java
+++ src/main/java/com/cg/invoiceservice/InvoiceService.java
@@ -1,5 +1,13 @@
 package com.cg.invoiceservice;
 
 public class InvoiceService {
+    private final double COST_PER_KM=10;
+    private final int COST_PER_MINUTE=1;
+
+    public double calcFare(double distance, int timeInMinute) {
+        double fare=distance*COST_PER_KM+timeInMinute*COST_PER_MINUTE;
+        if(fare<5) return 5;
+        else return fare;
+    }
 
 }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -5,6 +5,19 @@ import org.junit.Test;
 public class InvoiceServiceTest {
     @Test
     public void whenGivenTimeAndDistanceCalculateFare() {
+        InvoiceService invoiceService= new InvoiceService();
+        double distance=10; int time=10;
+        double fare=invoiceService.calcFare(distance,time);
+        Assert.assertEquals(110,fare,0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTimeReturnMinimumFare() {
+        InvoiceService invoiceService= new InvoiceService();
+        double distance=0.2;int time=1;
+        double fare=invoiceService.calcFare(distance,time);
+        Assert.assertEquals(5,fare,0);
+
 
     }
 }
